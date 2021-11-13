00fa2cc8d63c7894a23578ef9cbed242e9ea3af7
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-11-13 19:49:14 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var hello= "hello world i am hazel";
+console.log(hello);
+
+// String type
+let name = "john" ;
+console.log("my name is " + name);
+console.log("data type of "+ name + " is " + (typeof name));
+
+// Numbers type
+
+let marks = 56;
+console.log("marks of john is " + marks)
+console.log( "data type of marks is " + typeof marks);
+
+
+// boolean
+let win= true;
+console.log("data type is " + typeof win);
+
+// Object literals
+let stMarks = {
+ harry : 76,
+ john: 48,
+ foek: 57
+}
+console.log("data type of stMarks is " + typeof stMarks);
