00d9077892e6c57bd8fcc6949af5d57add985657
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-11-26 19:49:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -481,5 +481,8 @@ let element = document.createElement('li');
 let text = document.createTextNode('I am a text node');
 element.appendChild(text)
 
-
+// Add a class name to the li element
+element.className = 'childul';
+element.id = 'createdLi';
+element.setAttribute('title', 'mytitle');
 
